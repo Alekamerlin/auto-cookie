40a5d2645d75cd38aec81058b740a6040bfcebe9
alekamerlin <alekamerlin@gmail.com>
2023-03-05 00:28:25 +0200

    Implement automatic clicks to the golden cookie

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+// Atomatic clicks to the big cookie
 setInterval(() => document.getElementById('bigCookie').click(), 50);
+
+// Atomatic clicks to the golden cookies
+setInterval(() => {
+  Array.from(document.getElementById('shimmers').children).forEach((el) => {
+    el.click();
+  });
+}, 50);
 
